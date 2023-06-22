a7603d4ef2f1e609cdce61bed3684a0d87ade871
Valisher Muminov <valishaermuminov@gmail.com>
2023-06-22 23:58:48 +0900

    Exercise done

--- 8_kyu.js
+++ 8_kyu.js
@@ -151,3 +151,21 @@
 // console.log(typeof student1); // obj
 // console.log(typeof Students); // function
 // // console.log(typeof constructor);
+
+/* 5. Clock shows h hours, m minutes and s seconds after midnight.
+Your task is to write a function which returns the time since midnight in milliseconds.
+Example:
+h = 0
+m = 1
+s = 1
+result = 61000 */
+function past(h, m, s){
+  const milliseconds = (h * 3600 + m * 60 + s) * 1000;
+  return milliseconds;
+}
+let h = 0;
+let m = 1;
+let s = 1;
+
+let result = past(h, m, s);
+console.log(result); // Output: 61000
